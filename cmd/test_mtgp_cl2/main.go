f7965a27aca7413e85f7f39fd902f76d2b89e2f9
Xuanyao Fong <xuanyao.fong@gmail.com>
2018-12-31 11:44:42 +0800

    Fix MTGP based RNG - Number of RNGs in with Mersenne prime 23209 increased.
    - Added Perl script to postprocess MTGPDC output
    
    - Updated engine and rng.go
    
    - Make initRNG to be private in all packages
    
    - Added test to cycle RNG
    
    Pre-compile and test of test_mtgp_cl

--- cmd/test_mtgp_cl2/main.go
+++ cmd/test_mtgp_cl2/main.go
@@ -15,8 +15,10 @@ import (
 
 var (
 	d_length      = flag.Int("size", 1024, "Total number of random numbers to generate")
+	n_cycles      = flag.Int("cycles", 5, "Total number of random number generation cycles")
 	r_seed        = flag.Uint("seed", 0, "Seed value of RNG")
 	d_dump        = flag.Bool("dump", false, "Whether to dump generated values to screen")
+	d_norm        = flag.Bool("norm", false, "Whether to generate normally distributed numbers")
 	Flag_platform = flag.Int("platform", 0, "Specify OpenCL platform")
 	Flag_gpu      = flag.Int("gpu", 0, "Specify GPU")
 )
@@ -64,7 +66,13 @@ func main() {
 		fmt.Println("Results before execution: ", resultsArr[0])
 	}
 
-	rng.Uniform(output.DevPtr(0), d_size, []*cl.Event{output.GetEvent(0)})
+	for idx := 0; idx < *n_cycles; idx++ {
+		if *d_norm {
+			rng.Normal(output.DevPtr(0), d_size, []*cl.Event{output.GetEvent(0)})
+		} else {
+			rng.Uniform(output.DevPtr(0), d_size, []*cl.Event{output.GetEvent(0)})
+		}
+	}
 
 	resultsSlice = output.HostCopy()
 	resultsArr = resultsSlice.Host()
@@ -73,7 +81,15 @@ func main() {
 		fmt.Println("Results after execution: ", resultsArr[0])
 	}
 
-	fOut, fErr := os.Create("uniform_bytes.bin")
+        var fOut *os.File
+        var fErr error
+
+        if *d_norm {
+        	fOut, fErr = os.Create("norm_bytes.bin")
+        } else {
+        	fOut, fErr = os.Create("uniform_bytes.bin")
+        }
+
 	if fErr != nil {
 		panic(fErr)
 	}
@@ -88,53 +104,6 @@ func main() {
 	}
 
 	nn, wrErr := wr.Write(outBytes.Bytes())
-	if wrErr != nil {
-		fmt.Println("bufio.Write failed: ", wrErr)
-	} else {
-		wr.Flush()
-		fmt.Println("Wrote ", nn, "bytes to file")
-	}
-	fOut.Close()
-
-	opencl.Recycle(output)
-
-	fmt.Printf("Re-initializing MTGP RNG and generate normally distributed numbers... \n")
-
-	rng.Init(&seed, nil)
-
-	output = opencl.Buffer(1, [3]int{d_size, 1, 1})
-
-	resultsSlice = output.HostCopy()
-	resultsArr = resultsSlice.Host()
-
-	if *d_dump {
-		fmt.Println("Results before execution: ", resultsArr[0])
-	}
-
-	rng.Normal(output.DevPtr(0), d_size, []*cl.Event{output.GetEvent(0)})
-
-	resultsSlice = output.HostCopy()
-	resultsArr = resultsSlice.Host()
-
-	if *d_dump {
-		fmt.Println("Results after execution: ", resultsArr[0])
-	}
-
-	fOut, fErr = os.Create("norm_bytes.bin")
-	if fErr != nil {
-		panic(fErr)
-	}
-
-	wr = bufio.NewWriter(fOut)
-	outBytes = new(bytes.Buffer)
-	for _, v := range resultsArr[0] {
-		vErr := binary.Write(outBytes, binary.LittleEndian, v)
-		if vErr != nil {
-			fmt.Println("binary.Write failed: ", vErr)
-		}
-	}
-
-	nn, wrErr = wr.Write(outBytes.Bytes())
 	if wrErr != nil {
 		fmt.Println("bufio.Write failed: ", wrErr)
 	} else {
